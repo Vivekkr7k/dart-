c235a6b89d6808d073af10ceaa71ed2f0e899391
Vivekkr7k <vivekkr787067@gmail.com>
2022-05-29 03:04:03 +0530

    git push origin master

--- class.dart
+++ class.dart
@@ -19,15 +19,14 @@ class Phone{
       print(display);
       print(price);
     }
+    void turnOn(){
+    print("$name is turned on");
+    }
+
 }
 
 
-
- 
-
 void main(){
-
-
   Phone phone1=Phone();
   phone1.name = "apple";
   phone1.colour = "black";
@@ -35,10 +34,14 @@ void main(){
   phone1.processor = 650;
   phone1.display="amoled";
   phone1.price=15000;
+
   print("----------------------device details-----------------------");
   phone1.showOutput();
   print("----------------------device spec--------------------------");
   phone1.showOutputspec();
+
+
+  phone1.turnOn();
 
 
 
