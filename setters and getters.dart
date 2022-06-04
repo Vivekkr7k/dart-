92f67a9f6aae312ebea1921ec5ccfd5b0acd9944
Vivekkr7k <vivekkr787067@gmail.com>
2022-06-04 17:39:21 +0530

    git push origin master

---
+++ setters and getters.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+class Phone{
+  late String name;
+  late String color;
+
+  Phone(this.name , this.color);
+
+  String get getname => name;
+  set setname(String value) => name;
+
+}
+
+
+
+
+
+
+
+
+void main(){
+  var Phone1 = Phone("apple","balck");
+  print(Phone1.name);
+
+  Phone1.setname = "newname";
+  print(Phone1.getname);
+
+
+
+
+
+
+
+}
